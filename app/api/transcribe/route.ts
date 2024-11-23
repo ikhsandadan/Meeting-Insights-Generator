89b72a6fcf6964b78d660e7e0209bd9879a89ccc
Nashki <116878888+ikhsandadan@users.noreply.github.com>
2024-11-23 21:23:39 +0700

    Update route.ts

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -2,23 +2,36 @@ import { NextResponse } from 'next/server';
 
 const token = process.env.NEXT_PUBLIC_HUGGING_FACE_TOKEN;
 
+async function fetchWithRetry(url, options, retries = 3) {
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) throw new Error(response.statusText);
+        return await response.json();
+    } catch (error) {
+        if (retries > 0) {
+            console.log(`Retrying... (${3 - retries + 1})`);
+            return fetchWithRetry(url, options, retries - 1);
+        } else {
+            throw error;
+        }
+    }
+}
+
 export async function POST(request: Request) {
     try {
         // Get filename from request URL
         const { searchParams } = new URL(request.url);
         const fileUrl = searchParams.get('fileUrl'); // Cloudinary's url for the file
 
-        const response = await fetch(
-            "https://api-inference.huggingface.co/models/openai/whisper-large-v3",
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/octet-stream",
-                },
-                method: "POST",
-                body: JSON.stringify({ url: fileUrl }),
-            }
-        );
+        const response = await fetchWithRetry("https://api-inference.huggingface.co/models/openai/whisper-large-v3", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/octet-stream",
+            },
+            method: "POST",
+            body: JSON.stringify({ url: fileUrl }),
+        });
+
 
         const result = await response.json();
 
