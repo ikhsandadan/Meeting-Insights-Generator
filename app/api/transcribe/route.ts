98fa3b9289c27f6ddf583fe4c31f0289297e12f9
Nashki <116878888+ikhsandadan@users.noreply.github.com>
2024-11-23 20:24:17 +0700

    Update route.ts

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -4,12 +4,20 @@ const token = process.env.NEXT_PUBLIC_HUGGING_FACE_TOKEN;
 
 export async function POST(request: Request) {
     try {
-        // Get filename from request URL
+        // Get fileUrl from request URL
         const { searchParams } = new URL(request.url);
-        const fileUrl = searchParams.get('fileUrl'); // Cloudinary's url for the file
+        const fileUrl = searchParams.get('fileUrl'); // Cloudinary's URL for the file
 
-        console.log(fileUrl);
+        if (!fileUrl) {
+            return NextResponse.json(
+                { error: 'URL not provided' },
+                { status: 400 }
+            );
+        }
 
+        console.log('File URL:', fileUrl);
+
+        // Fetch from Hugging Face API
         const response = await fetch(
             "https://api-inference.huggingface.co/models/openai/whisper-large-v3",
             {
@@ -22,13 +30,17 @@ export async function POST(request: Request) {
             }
         );
 
+        if (!response.ok) {
+            throw new Error(`Hugging Face API error: ${response.statusText}`);
+        }
+
         const result = await response.json();
 
-        console.log(result);
+        console.log('Hugging Face API response:', result);
 
         return NextResponse.json(result);
     } catch (error: unknown) {
-        console.error(error);
+        console.error('Error during the transcription process:', error);
 
         // Type assertion to narrow down the unknown type
         let errorMessage = 'Unknown error';
